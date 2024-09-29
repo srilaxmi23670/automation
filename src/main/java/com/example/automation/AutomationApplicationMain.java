b68dcd257b8647cd9f7c4ce90f9dac35a8d9baaf
Srilaxmi <srilaxmi.n@turtlemint.com>
2024-09-29 17:07:52 +0530

    add project

---
+++ src/main/java/com/example/automation/AutomationApplicationMain.java
@@ -0,0 +1,13 @@
+package com.example.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationApplication.class, args);
+	}
+
+}
